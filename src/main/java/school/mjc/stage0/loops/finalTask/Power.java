b124021754e10e09b78f8c26eff2d29f17f33bc5
8ar7ar <5ar7ar@mail.ru>
2023-10-24 22:11:26 +0500

    tage0-module4-loops-taskFinal "1st attempt" "locally tested"

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        double result = 1;
+        if (power >=0)for (int i = 1; i <= power; i++)result *= numberToPrint;
+        else for (int i = 1; i <= power*-1; i++) result /= numberToPrint;
+        System.out.println((int)result);
     }
 
     public static void main(String[] args) {
